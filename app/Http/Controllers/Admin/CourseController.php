50639e77a7832fae36791b6a8f682ffa47d9492e
tuaha-alim <tuahaalim@gmail.com>
2019-02-27 16:44:02 +0600

    First commit

---
+++ app/Http/Controllers/Admin/CourseController.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Course;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class CourseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $courses = Course::latest()->paginate(10);
+        return view('admin.course.index',compact('courses'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.course.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+
+            'course_name' => 'required',
+            'total_class' => 'required',
+            'class_hour' => 'required',
+            'price' => 'required',
+
+
+
+        ]);
+
+        $courses = new Course();
+
+        $courses->course_name = $request->course_name ;
+        $courses->total_class = $request->total_class ;
+        $courses->class_hour = $request->class_hour ;
+        $courses->price = $request->price;
+        $courses->save();
+        $notification = array(
+            'ttitle' => 'Course Details Successfully Added',
+            'tmsg' => 'You have added a Course Details',
+            'ticon' => 'success',
+        );
+        return back()->with($notification);
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function deletecourse(Request $request){
+
+        Course::find ( $request->id )->delete ();
+
+        return response ()->json ();
+
+    }
+}
---
+++ database/migrations/2019_02_23_122535_create_courses_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->string('course_name')->nullable();
+            $table->integer('price')->nullable();
+            $table->string('total_class')->nullable();
+            $table->string('class_hour')->nullable();
+            $table->string('slug')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
